0a0f5ed7a484892b102de1dea654c363605ebbee
akbarahmed <akbar501@gmail.com>
2015-07-10 11:18:32 -0700

    add placeholder content to Fragment

--- app/src/main/java/io/exponential/mdnavigation/MainActivity.java
+++ app/src/main/java/io/exponential/mdnavigation/MainActivity.java
@@ -1,9 +1,7 @@
 package io.exponential.mdnavigation;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -11,9 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
